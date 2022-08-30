fbec94330d9855d0eb074d23e99e75171ee4fae7
Javier Cepeda <erjavierc@gmail.com>
2022-08-30 07:07:59 -0400

    intro-react

---
+++ ToDoCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodoCounter = () => {
+    return (
+        <h2>You have finished 2 of 3 ToDos</h2>
+    );
+}
+
+export default TodoCounter;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
+import TodoCounter from "../ToDoCounter";
 // import './App.css';
 
 const todos = [
-  {
-    text: 'Cut onion', completed: false
-  },
+  { text: 'Cut onion', completed: false},
   { text: 'take classes', completed: false },
   { text: 'buy milk', completed: false}
 ]
 function App() {
   return (
     <React.Fragment>
-      <todoCounter />
-      <h2>You have finished 2 of 3 ToDos</h2>
-      <todoSearch />
+     {/* { <todoCounter />} */}
+      {/* {<todoSearch />} */}
       <input type="text" placeholder="Onion"/>
-      <todoList>
+      <button> + </button>
+{/* {      <todoList>
         {todos.map(todo => (
           <todoItem />
         ))}
-      </todoList>
-      <CreateTodoButton />
+      </todoList>} */}
+      {/* {<CreateTodoButton />} */}
     </React.Fragment>
   );
 }
