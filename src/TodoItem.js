e7371f056427ff6ec07f41003d8860ac22777aba
Javier Cepeda <erjavierc@gmail.com>
2022-08-30 09:12:44 -0400

    intro-react

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,11 +3,21 @@ import './TodoItem.css';
 
 
 const TodoItem = (props) => {
+    const onComplete = () => {
+        alert('Here is complete ' + props.text);
+    }
+    const onDelete = () => {
+        alert('Here is deleted ' + props.text);
+    }
     return(
         <li className="TodoItem">
-            <span className="{`Icon Icon-check ${props.completed && 'Icon-check--active'}`}">√</span>
+            <span className="{`Icon Icon-check ${props.completed && 'Icon-check--active'}`}"
+            onClick={onComplete}
+            >√</span>
             <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>{props.text}</p>
-            <span className="Icon Icon-delete">X</span>
+            <span className="Icon Icon-delete"
+            onClick={onDelete }
+            >X</span>
         </li>
     );
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,18 @@
 import React from "react";
 import './TodoSearch.css';
 
+
 const TodoSearch = () => {
+    const onSearchValueChange = (event) =>{
+        console.log(event.target.value)
+    }
     return (
-        <input className="TodoSearch" type="text" placeholder="buy onions" />
+        <input 
+        className="TodoSearch" 
+        type="text" 
+        placeholder="buy onions"
+        onChange={onSearchValueChange}
+         />
     );
 }
 
