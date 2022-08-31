6587445189fb393841db64231f92e0d7154f6841
Javier Cepeda <erjavierc@gmail.com>
2022-08-31 07:51:08 -0400

    intro-react

--- src/App.js
+++ src/App.js
@@ -18,6 +18,19 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  let searchedTodos = [];
+
+  if (!searchedTodos.length >= 1){
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+    
+  }
+
   return (
     <React.Fragment>
      <TodoCounter
@@ -29,7 +42,7 @@ function App() {
          setSearchValue={setSearchValue}
       />
       {<TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
         ))}
       </TodoList>}
